6c8f745936473b3554d33f22ce72189f2765ae9c
Andriy Kryvtsun <andy@akryvtsun.com>
2020-05-02 18:37:41 +0300

    fix prime number check

--- src/main/java/com/ajax/task2/PrimeCalculatorContract.java
+++ src/main/java/com/ajax/task2/PrimeCalculatorContract.java
@@ -11,6 +11,7 @@ abstract class PrimeCalculatorContract {
     /* checks whether an int is prime or not. */
     public static boolean isPrime(int n) {
         //check if n is a multiple of 2
+        if (n == 2) return true;
         if (n % 2 == 0) return false;
         //if not, then just check the odds
         for (int i = 3; i * i <= n; i += 2) {
